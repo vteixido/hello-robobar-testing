f2897207702de19cd4c19fb52c059c6e8aa61933
Javier Rodriguez <profesor@sinensia.com>
2022-02-08 09:33:07 +0100

    Initial import

---
+++ src/test/java/com/sinensia/hellorobobartesting/MainPage.java
@@ -0,0 +1,22 @@
+package com.sinensia.hellorobobartesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// https://www.jetbrains.com/
+public class MainPage {
+    @FindBy(css = "a.wt-button_mode_primary")
+    public WebElement seeAllToolsButton;
+
+    @FindBy(xpath = "//div[contains(@class, 'menu-main__item') and text() = 'Developer Tools']")
+    public WebElement toolsMenu;
+
+    @FindBy(css = "[data-test='menu-main-icon-search']")
+    public WebElement searchButton;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
